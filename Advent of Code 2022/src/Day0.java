09ef9229a493de132990e3d01c277005e9cd96e1
loicetter <loicetter@172.20.10.2>
2022-11-30 15:06:20 +0100

    initial commit

---
+++ Advent of Code 2022/src/Day0.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Day0 implements AoCTask {
+	
+	int[] input = new int[2000];
+	
+	@Override
+	public void readInput(Scanner scan) {
+		for(int i = 0; i < 2000; i++) {
+			input[i] = scan.nextInt();
+		}
+	}
+
+	@Override
+	public String task1() {
+		return "";
+	}
+
+	@Override
+	public String task2() {
+		return "";
+	}
+
+}
---
+++ Advent of Code 2022/src/createTask.java
@@ -0,0 +1,68 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class createTask {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		
+		// Get the day
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd");
+		LocalDate localDate = LocalDate.now();
+		String daynumber = dtf.format(localDate);
+		System.out.println(daynumber);
+		
+		
+		createClass(daynumber, "src/");
+		createInput(daynumber, "inputs/");
+		
+		
+		
+
+	}
+	static void createClass(String daynumber, String path) throws FileNotFoundException {
+		String filename = "src/Day" + daynumber + ".java";
+		PrintStream out = new PrintStream(filename);
+		
+		String basecode = 
+				"import java.util.Scanner;\n"
+				+ "\n"
+				+ "public class Day"
+				+ daynumber 
+				+ " implements AoCTask {\n"
+				+ "	\n"
+				+ "	int[] input = new int[2000];\n"
+				+ "	\n"
+				+ "	@Override\n"
+				+ "	public void readInput(Scanner scan) {\n"
+				+ "		for(int i = 0; i < 2000; i++) {\n"
+				+ "			input[i] = scan.nextInt();\n"
+				+ "		}\n"
+				+ "	}\n"
+				+ "\n"
+				+ "	@Override\n"
+				+ "	public String task1() {\n"
+				+ "		return \"\";\n"
+				+ "	}\n"
+				+ "\n"
+				+ "	@Override\n"
+				+ "	public String task2() {\n"
+				+ "		return \"\";\n"
+				+ "	}\n"
+				+ "\n"
+				+ "}";
+		out.println(basecode);
+		out.close();
+	}
+	static void createInput(String daynumber, String path) throws FileNotFoundException {
+		String filename = path + "Day" + daynumber + "";
+		PrintStream out = new PrintStream(filename);
+		
+		String basecode = "";
+		System.out.println(basecode);
+		out.println(basecode);
+		out.close();
+	}
+}
