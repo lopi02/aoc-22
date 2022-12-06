cdd9366e4d1d85a5e5cc05ab7362c2f4948537f4
lopi02 <loic@zummedium.ch>
2022-12-06 08:49:02 +0100

    - day06: created a method for the reusable code

--- Advent of Code 2022/src/Day06.java
+++ Advent of Code 2022/src/Day06.java
@@ -14,43 +14,40 @@ public class Day06 implements AoCTask {
 		}
 	}
 
+	public int markerAt(String str, int pos) {
+		ArrayList<Character> letters = new ArrayList<Character>();
+		for(int j = 0; j < str.length(); j++) {
+				char let = str.charAt(j);
+				while(letters.contains(let)) {
+					letters.remove(0);
+				}
+				letters.add(let);
+				if (letters.size() == pos) return ( j+1);
+		}
+		return -1;
+	}
+	
 	@Override
 	public String task1() {
 		/*
 		 * - the first position where the four most recently received characters were all different
 		 */
+		String out = " ";
 		for (int i = 0; i < input.length; i++) {
 			String str = input[i];
-			ArrayList<Character> letters = new ArrayList<Character>();
-			for(int j = 0; j < str.length(); j++) {
-					char let = str.charAt(j);
-					while(letters.contains(let)) {
-						letters.remove(0);
-					}
-					letters.add(let);
-					if (letters.size() == 4) return ""+( j+1);
-			}
-
-		}
-		return "";
+				out = out + " " + markerAt(str, 4);
+			};
+		return out;
 	}
 
 	@Override
 	public String task2() {
+		String out = " ";
 		for (int i = 0; i < input.length; i++) {
 			String str = input[i];
-			ArrayList<Character> letters = new ArrayList<Character>();
-			for(int j = 0; j < str.length(); j++) {
-					char let = str.charAt(j);
-					while(letters.contains(let)) {
-						letters.remove(0);
-					}
-					letters.add(let);
-					if (letters.size() == 14) return ""+( j+1);
-			}
-
-		}
-		return "";
+				out = out + " " + markerAt(str, 14);
+			};
+		return out;
 	}
 
 }
